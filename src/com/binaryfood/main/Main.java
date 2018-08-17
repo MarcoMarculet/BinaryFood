18b98c8a902ab3044cbb7c63dde3d7f4e62a695b
MarcoMarculet <marculet.m@gmail.com>
2018-08-17 19:28:41 +0300

    show diff

--- src/com/binaryfood/main/Main.java
+++ src/com/binaryfood/main/Main.java
@@ -2,6 +2,6 @@ package com.binaryfood.main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello BinaryFood");
+        System.out.println("Hello");
     }
 }
